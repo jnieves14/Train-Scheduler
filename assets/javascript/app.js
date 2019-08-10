4770b1573496f2203c73d937e205eb01523f55c7
Jannah June Nieves <jannahjune@gmail.com>
2019-08-10 08:18:50 -0700

    variable names corrected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,41 +17,41 @@ var firebaseConfig = {
   // Create a variable to reference the database
   var database = firebase.database();
   
-  // Create an on click function that adds trains to the top table
+  // OnClick function that adds the train schedule to the Current Train Schedule
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
   
-    // create variables with the user input from form
+    // Variables created for the user input
     var trainName = $("#train-name-input").val().trim();
     var trainDestination = $("#destination-input").val().trim();
     var firstTrainTime= $("#first-train-input").val().trim();
     var trainFrequency = $("#frequency-input").val().trim();
   
-    // create a temporary object for holding the new train data
-    var newTrn = {
+    // Varibale to hold the new train's information
+    var newTrain = {
       name: trainName,
       destination: trainDestination,
       firstTime: firstTrainTime,
       frequency: trainFrequency
     };
   
-    // upload the new train data to the database
-    database.ref().push(newTrn);
+    // push the new trains information to the database
+    database.ref().push(newTrain);
   
     // console log the values that were just pushed to the database
-    console.log(newTrn.name);
-    console.log(newTrn.destination);
-    console.log(newTrn.firstTime);
-    console.log(newTrn.frequency);
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTime);
+    console.log(newTrain.frequency);
   
-    // clear the form values after values have been stored
+    // after values have been stored, clear the values from the form so another input can be made
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#first-train-input").val("");
     $("#frequency-input").val("");
   });
   
-  // create a firebase event for adding the data from the new trains and then populating them in the DOM.
+  // firebase event for adding all new trains' data and populating them on the comain
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
     console.log(childSnapshot.val());
   
@@ -67,22 +67,21 @@ var firebaseConfig = {
     console.log(firstTrainTime);
     console.log(trainFrequency);
   
-    // process for calculating the Next Arrival and Minutes Away fields...
-    // make sure the first train time is after the eventual current time
+    // calculate the Next Arrival and Minutes Away fields
     var firstTrainTimeConv = moment(firstTrainTime, "hh:mm a").subtract(1, "years");
     // store variable for current time
     var currentTime = moment().format("HH:mm a");
     console.log("Current Time:" + currentTime);
     // store variable for difference of current time and first train time
-    var trnTimeCurrentTimeDiff = moment().diff(moment(firstTrainTimeConv), "minutes");
+    var trainTimeCurrentTimeDiff = moment().diff(moment(firstTrainTimeConv), "minutes");
     // store the time left
-    var timeLeft = trnTimeCurrentTimeDiff % trainFrequency;
+    var timeLeft = trainTimeCurrentTimeDiff % trainFrequency;
     // calculate and store the minutes until next train arrives
     var minutesAway = trainFrequency - timeLeft;
     // calculate the next arriving train
     var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
   
-    // add the data into the DOM/html
+    // add the data
     $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
   });
 
